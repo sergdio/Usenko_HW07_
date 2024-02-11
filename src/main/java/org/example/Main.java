cfc8a4080c4bc1f96f6d7aca973f219a50fa6eab
Serhii Usenko <sergdio@ukr.net>
2024-02-11 17:23:48 +0200

    new metod

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,28 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello DDDDio!");
+        List<String> inputData = List.of("5", "+", "5");
+        Integer result = 10;
+        String resultString = prepareResults(inputData, result);
+        System.out.println(resultString);
+    }
+
+    public static String prepareResults(List<String> inputData, Integer result) {
+        StringBuilder formattedResult = new StringBuilder();
+
+        for (int i = 0; i < inputData.size() - 1; i++) {
+            formattedResult.append(inputData.get(i));
+        }
+
+        if (inputData.get(inputData.size() - 2).equals("+") || inputData.get(inputData.size() - 2).equals("-")) {
+            formattedResult.append(inputData.get(inputData.size() - 2)).append(inputData.get(inputData.size() - 1));
+        }
+
+        formattedResult.append("=").append(result);
+
+        return formattedResult.toString();
     }
 }
